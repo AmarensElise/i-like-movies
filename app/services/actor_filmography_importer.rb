6e780f3a1fb7dc2b7093d945f0461794f8f73bec
Amarens <amarens.jonker@gmail.com>
2025-12-21 17:32:51 +0000

    Create actor_filmography_importer.rb

---
+++ app/services/actor_filmography_importer.rb
@@ -0,0 +1,31 @@
+# app/services/tmdb/actor_filmography_importer.rb
+class Tmdb::ActorFilmographyImporter
+  def initialize(actor)
+    @actor = actor
+  end
+
+  def call
+    credits = TmdbService.fetch_person_credits(@actor.tmdb_id)
+    import_cast_roles(credits['cast'])
+  end
+
+  private
+
+  def import_cast_roles(cast_movies)
+    cast_movies.each do |movie_data|
+      movie = Movie.find_or_create_by!(tmdb_id: movie_data['id']) do |m|
+        m.title        = movie_data['title']
+        m.release_date = movie_data['release_date']
+        m.poster_path  = movie_data['poster_path']
+        m.vote_average = movie_data['vote_average']
+      end
+
+      Role.find_or_create_by!(
+        actor: @actor,
+        movie: movie
+      ) do |role|
+        role.character = movie_data['character']
+      end
+    end
+  end
+end
