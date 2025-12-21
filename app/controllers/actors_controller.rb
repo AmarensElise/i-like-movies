8ba6c2920443b09383a6f7e0b87341e4bda0f373
Amarens <amarens.jonker@gmail.com>
2025-12-21 13:57:07 +0000

    updates

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,18 @@
 class ActorsController < ApplicationController
+  def index
+    @actors = Actor.all.order(:name)
+  end
+
   def show
+    @actor = Actor.find(params[:id])
+    @roles = @actor.roles.includes(:movie).order('movies.release_date DESC')
+
+    # Fetch additional details from TMDB API
+    begin
+      @actor_details = TmdbService.fetch_person(@actor.tmdb_id)
+    rescue => e
+      Rails.logger.error("Error fetching actor details: #{e.message}")
+      @actor_details = nil
+    end
   end
 end
